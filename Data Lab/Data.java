35a535c297d2e0d0409f6de7ac4822745bf86554
ChloeBarlow <barlow4061@mydusd.org>
2026-01-31 20:20:19 -0800

    Adding Code V5
    Updated the logic for how the average & standard deviation for the whole dataset is calculated and an additional feature where the user can input a minimum number of reviews for a game to be considered in determining which game is the most well received

--- Data Lab/Data.java
+++ Data Lab/Data.java
@@ -1,10 +1,10 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-
+import java.util.Scanner;
 
 public class Data {
-    
+
     // Instance Variables
     private String gameTitle;
     private double userRating;
@@ -12,7 +12,7 @@ public class Data {
     private String genre;
 
     // Default constructor
-    public Data(){
+    public Data() {
         gameTitle = "N/A";
         userRating = 0;
         releaseDate = "";
@@ -20,7 +20,7 @@ public class Data {
     }
 
     // Initializing constructor
-    public Data(String title, double rating, String date, String genre){
+    public Data(String title, double rating, String date, String genre) {
         this.gameTitle = title;
         this.userRating = rating;
         this.releaseDate = date;
@@ -28,37 +28,38 @@ public class Data {
     }
 
     // Accessors
-    public String getTitle(){
+    public String getTitle() {
         return gameTitle;
     }
 
-    public double getUserRating(){
+    public double getUserRating() {
         return userRating;
     }
 
-    public String getDate(){
+    public String getDate() {
         return releaseDate;
     }
 
-    public String getGenre(){
+    public String getGenre() {
         return genre;
     }
 
     // Methods
-    public String toString(){
-        return("Game: " + gameTitle + " | User Rating: " + userRating + " | Release date: " + releaseDate + " | Genre: " + genre);
+    public String toString() {
+        return ("Game: " + gameTitle + " | User Rating: " + userRating + " | Release date: " + releaseDate
+                + " | Genre: " + genre);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Data[] dataList = new Data[513250];
 
         String csvFile = "metacritic_pc_games.csv";
         String line = "";
         int index = 0;
-        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))){
-            while ((line = br.readLine()) != null){
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            while ((line = br.readLine()) != null) {
                 String[] values = line.split(";", -1); // -1 to keep trailing empty strings
-                if (values.length == 4){
+                if (values.length == 4) {
                     String title = values[0];
                     String ratingStr = values[3].trim();
                     double rating;
@@ -79,22 +80,23 @@ public class Data {
                     index++;
                 }
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
-        /* Print all Ratings
-        for (Data data : dataList){
-            System.out.println(data.toString());
-        }
-        */
+        /*
+         * Print all Ratings
+         * for (Data data : dataList){
+         * System.out.println(data.toString());
+         * }
+         */
 
         // Find total number of unique game titles
         String currentTitle = "";
         int numTitles = 0;
-        for (Data data : dataList){
-            if (data != null){
-                if (!data.getTitle().equals(currentTitle)){
+        for (Data data : dataList) {
+            if (data != null) {
+                if (!data.getTitle().equals(currentTitle)) {
                     numTitles++;
                     currentTitle = data.getTitle();
                 }
@@ -104,29 +106,39 @@ public class Data {
 
         // Creating variables needed for average and standard deviation calculations
         double totalRatings = 0;
-        double averageRating = 0;
+        double overallAvgRating = 0;
         double ratingDiff = 0;
         double sumRatingDiff = 0;
         double ratingStdDev = 0;
-        
-        // Find sums of user ratings by game titles (+ store titles, genres, and dates)
+
+        // Arrays to hold sums of ratings, individual average ratings, titles, genres, dates, and number of reviews
         double[] ratingSums = new double[numTitles];
+        double[] ratingAvgs = new double[numTitles];
         String[] titles = new String[numTitles];
         String[] genres = new String[numTitles];
         String[] dates = new String[numTitles];
+        int[] numReviews = new int[numTitles];
+
+        // Find sums of user ratings by game titles (+ store number of reviews, titles, genres, and dates)
         titles[0] = dataList[0].getTitle();
         genres[0] = dataList[0].getGenre();
         dates[0] = dataList[0].getDate();
         currentTitle = dataList[0].getTitle();
         int titleIndex = 0;
+        int numOfReviews = 0;
         for (int i = 0; i < dataList.length; i++) {
             Data data = dataList[i];
-            if (data == null) continue;
+            if (data == null)
+                continue;
             if (data.getTitle().equals(currentTitle)) {
                 ratingSums[titleIndex] += data.getUserRating();
+                numOfReviews++;
             } else {
+                numReviews[titleIndex] = numOfReviews;
+                numOfReviews = 1;
                 titleIndex++;
-                if (titleIndex >= numTitles) break;
+                if (titleIndex >= numTitles)
+                    break;
                 currentTitle = data.getTitle();
                 titles[titleIndex] = currentTitle;
                 genres[titleIndex] = data.getGenre();
@@ -134,33 +146,58 @@ public class Data {
                 ratingSums[titleIndex] += data.getUserRating();
             }
         }
-        
-        // Calculate average user rating by game titles
-        for (double sum : ratingSums) {
-            totalRatings += sum;
+
+        // Calculate average ratings for each title
+        for (int i = 0; i < numTitles; i++) {
+            if (numReviews[i] > 0) {
+                ratingAvgs[i] = ratingSums[i] / numReviews[i];
+            } else {
+                ratingAvgs[i] = 0;
+            }
         }
 
-        averageRating = totalRatings / numTitles;
-        System.out.println("The average user rating by game titles is: " + averageRating);
+        // Calculate average user rating by game titles
+        for (double avg : ratingAvgs) {
+            totalRatings += avg;
+        }
+
+        overallAvgRating = totalRatings / numTitles;
+        System.out.println("The average user rating by game titles is: " + overallAvgRating);
 
         // Calculate standard deviation of user ratings by game titles
-        for (double sum : ratingSums) {
-            ratingDiff = sum - averageRating;
+        for (double avg : ratingAvgs) {
+            ratingDiff = avg - overallAvgRating;
             sumRatingDiff += ratingDiff * ratingDiff;
         }
         ratingStdDev = Math.sqrt(sumRatingDiff / numTitles);
         System.out.println("The standard deviation of user ratings by game titles is: " + ratingStdDev);
 
-        // Find which game title has the highest user rating sum with its release date & genre
-        double highestRating = ratingSums[0];
+        // Find which game title has the highest user rating average with its release date & genre
+        double highestRating = ratingAvgs[0];
         String highestTitle = titles[0];
         String highestDate = dates[0];
         String highestGenre = genres[0];
-        for (Data data : dataList){
-            if (data != null){
-                for (int i = 0; i < ratingSums.length; i++){
-                    if (ratingSums[i] > highestRating){
-                        highestRating = ratingSums[i];
+
+        Scanner scanner = new Scanner(System.in); // Get user input for minimum number of reviews to consider
+        System.out.print("Enter the minimum number of reviews a game must have to be considered (choose between 1 and 4070): ");
+        int userInput = scanner.nextInt();
+
+        // Validate user input
+        while (userInput < 1 || userInput > 4070) { // 4070 is the max number of reviews for a single game in the dataset
+            System.out.println("Invalid input. Please enter a number between 1 and 4070.");
+            System.out.print("Enter the minimum number of reviews a game must have to be considered: ");
+            userInput = scanner.nextInt();
+            if (userInput >= 1 && userInput <= 4070) {
+                break;
+            }
+        }
+
+        // Find highest rated game title that meets the user input criteria
+        for (Data data : dataList) {
+            if (data != null) {
+                for (int i = 0; i < ratingAvgs.length; i++) {
+                    if (ratingAvgs[i] > highestRating && numReviews[i] >= userInput) {
+                        highestRating = ratingAvgs[i];
                         highestTitle = titles[i];
                         highestDate = dates[i];
                         highestGenre = genres[i];
@@ -168,10 +205,11 @@ public class Data {
                 }
             }
         }
+        scanner.close();
 
         // Calculate how many standard deviations above the average the highest rating is
-        double numStdDevs = (highestRating - averageRating) / ratingStdDev;
-        System.out.println("The game title with the highest user rating sum is: [" + highestTitle + "] with a sum of [" + highestRating + "], released in [" 
-                            + highestDate + "] in the genre of [" + highestGenre + "].\nIt is [" + numStdDevs + "] standard deviations above the average.");
+        double numStdDevs = (highestRating - overallAvgRating) / ratingStdDev;
+        System.out.println("The game title with the highest user rating average is: [" + highestTitle + "] with an average\nof ["+ highestRating + "], released in ["
+                + highestDate + "] in the [" + highestGenre + "].\nIt is [" + numStdDevs+ "] standard deviations above the overall average in the dataset.");
     }
 }
